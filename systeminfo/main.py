48628c6150e8e8e570fdfa469e756101cd6d951a
Crane <95408805+CraneWvs@users.noreply.github.com>
2023-01-30 22:01:54 +0000

    editing

---
+++ systeminfo/main.py
@@ -0,0 +1,19 @@
+# This program should display:
+# 1. name of machine
+# 2. operating system name
+# 3. operating system version
+# 4. number of cpu's
+# 5. amount of memory
+# 6. IP address of machine
+# ©UCD Semester II/ Modular
+# 7. Using the time taken to execute a simple numerical task, compute a CPU strength score for the machine.
+# Use the timeit module and consider using the sample code 1 at the end, or some modiûcation of it.
+import socket
+def getMachineName():
+    print(socket.gethostname())
+
+def getOSName():
+
+
+print(socket.gethostname())
+# getMachineName()
