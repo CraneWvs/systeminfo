6829d8360a736c2c58675f1f67b80eb9dba0b406
cranewvs <yiyun_uibe@126.com>
2023-01-31 10:27:36 +0000

    modify main

--- systeminfo/main.py
+++ systeminfo/main.py
@@ -8,11 +8,23 @@
 # ©UCD Semester II/ Modular
 # 7. Using the time taken to execute a simple numerical task, compute a CPU strength score for the machine.
 # Use the timeit module and consider using the sample code 1 at the end, or some modiûcation of it.
+import os
+import platform
 import socket
 def getMachineName():
     print(socket.gethostname())
 
 def getOSName():
+    print("OS:", platform.uname().system )
+    print("OS version:", platform.uname().release)
+
+def cpuInformation():
+    print("CPU cores:", os.cpu_count())
+
+def machineIpAddress():
+    print("IP:", socket.gethostname(socket.gethostname()))
+
+
 
 
 print(socket.gethostname())
